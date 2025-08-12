e0f1e72f8524c77c77da53be169e9467f62156f0
Sandro <sandrocoelho2023a@gmail.com>
2025-08-12 14:10:15 -0300

    verificando o primeiro teste

--- tempo/meteorologia/tests.py
+++ tempo/meteorologia/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+
+class Testes(TestCase):
+    def test_1(self):
+        """Teste de abrir pagina index"""
+        c = Client()
+        response = c.get("")
+        self.assertEqual(response.status_code, 200)
